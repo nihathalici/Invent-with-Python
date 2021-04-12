788d850e0cb9445b64658ae6937fc64ebf944d4d
Nihat Halici <nihat.halici@gmail.com>
2021-04-12 00:18:12 -0700

    Create tic-tac-toe.py

---
+++ Tic-Tac-Toe/tic-tac-toe.py
@@ -0,0 +1,43 @@
+import random
+
+def drawBoard(board):
+    pass
+
+
+def inputPlayerLetter():
+    pass
+
+
+def whoGoesFirst():
+    pass
+
+
+def makeMove(board, letter, move):
+    pass
+
+
+def isWinner(bo, le):
+    pass
+
+
+def getBoardCopy(board):
+    pass
+
+
+def isSpaceFree(board, move):
+    pass
+
+
+def getPlayerMove(board):
+    pass
+
+
+def choose RandomMoveFromList(board, movesList):
+    pass
+
+
+def getComputerMove(board, computerLetter):
+    pass
+
+def isBoardFull(board):
+    pass
