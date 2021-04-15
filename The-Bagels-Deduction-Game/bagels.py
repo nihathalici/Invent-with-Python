cb773c8212867897b1b1af9075eb709421acb5ec
Nihat Halici <nihat.halici@gmail.com>
2021-04-15 02:55:24 -0700

    Update bagels.py

--- The-Bagels-Deduction-Game/bagels.py
+++ The-Bagels-Deduction-Game/bagels.py
@@ -1,7 +1,16 @@
 import random
 
+NUM_DIGITS = 3
+MAX_GUESS = 10
+
 def getSecretNum():
-    pass
+    # Returns a string of unique random digits that is NUM_DIGITS long.
+    numbers = list(range(10))
+    random.shuffle(numbers)
+    secretNum = ''
+    for i in range(NUM_DIGITS):
+        secretNum += str(numbers[i])
+    return secretNum
 
 def getClues(guess, secretNum):
     pass
