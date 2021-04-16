edd31e0661d6a8a25ff2d73fdcd751cd90e22d2f
Nihat Halici <nihat.halici@gmail.com>
2021-04-16 05:31:50 -0700

    Create sonar.py

---
+++ Sonar-Treasure-Hunt/sonar.py
@@ -0,0 +1,24 @@
+import random
+import sys
+import math
+
+def getNewBoard():
+    pass
+
+def drawBoard(board):
+    pass
+
+def getRandomChests(numChests):
+    pass
+
+def isOnBoard(x, y):
+    pass
+
+def makeMove(board, chests, x, y):
+    pass
+
+def enterPlayerMove(previousMoves):
+    pass
+
+def showInstructions:
+    pass
