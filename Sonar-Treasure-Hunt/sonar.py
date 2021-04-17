a879fc7e11c08f3b3b387ad88e49d48c92d6f6e9
Nihat Halici <nihat.halici@gmail.com>
2021-04-17 00:52:39 -0700

    Update sonar.py

--- Sonar-Treasure-Hunt/sonar.py
+++ Sonar-Treasure-Hunt/sonar.py
@@ -2,11 +2,52 @@ import random
 import sys
 import math
 
+
 def getNewBoard():
-    pass
+    # Create a new 60x15 data structure
+    board = []
+    for x in range(60): # The main list is a list of 60 lists.
+        board.append([])
+        for y in range(15): # Each list in the main list has 15 single-character strings.
+            # Use different characters for the ocean to make it more readable
+            if random.randint(0, 1) == 0:
+                board[x].append('~')
+            else:
+                board[x].append('`')
+    return board
+
 
 def drawBoard(board):
-    pass
+    # Draw the board data structure.
+    tensDigitsLine = '    ' # Initial space for the numbers down the left side of the board
+    for i in range(1, 6):
+        tensDigitsLine += (' ' * 9) + str(i)
+
+    # Print the numbers across the top of the board.
+    print(tensDigitsLine)
+    print('   ' + ('0123456789' * 6))
+    print()
+
+    # Print each of the 15 rows.
+    for row in range(15):
+        # Single-digit numbers need to be padded with an extra space.
+        if row < 10:
+            extraSpace = ' '
+        else:
+            extraSpace = ''
+
+        # Create the string for this row on the board.
+        boardRow = ''
+        for column in range(60):
+            boardRow += board[column][row]
+
+        print('%s%s %s %s' % (extraSpace, row, boardRow, row))
+
+    # Print the numbers across the bottom of the board.
+    print()
+    print(' ' + ('0123456789' * 6))
+    print(tensDigitsLine)
+
 
 def getRandomChests(numChests):
     pass
