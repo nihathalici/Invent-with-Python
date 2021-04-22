df044cd1c4939860b1001e1e494cdab1c4c7e58a
Nihat Halici <nihat.halici@gmail.com>
2021-04-22 00:53:17 -0700

    Create cipher.py

---
+++ Caesar-Cipher/cipher.py
@@ -0,0 +1,25 @@
+# Caesar Cipher
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+MAX_KEY_SIZE = len(SYMBOLS)
+
+def getMode():
+    pass
+
+
+def getMessage():
+    pass
+
+
+def getKey():
+    pass
+
+
+def getTranslatedMessage(mode, message, key):
+    pass
+
+
+mode = getMode()
+message = getMessage()
+key = getKey()
+print('Your translated text is:')
+print(getTranslatedMessage(mode, message, key))
