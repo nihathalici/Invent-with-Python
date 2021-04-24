23064427f4a6e6ddd4837316f083f738dc4f9fbb
Nihat Halici <nihat.halici@gmail.com>
2021-04-24 02:11:46 -0700

    Create reversegam.py

---
+++ The-Reversegam-Game/reversegam.py
@@ -0,0 +1,54 @@
+# Reversegam: a clone of Othello/Reversi
+import random
+import sys
+
+WIDTH = 8 # Board is 8 spaces wide.
+HEIGHT = 8 # Board is 8 spaces tall
+
+def drawBoard(board):
+    pass
+
+def getNewBoard():
+    pass
+
+def isValidMove(board, tile, xstart, ystart):
+    pass
+
+def isOnBoard(x, y):
+    pass
+
+def getBoardWithValidMoves(board, tile):
+    pass
+
+def getValidMoves(board, tile):
+    pass
+
+def getScoreOfBoard(board):
+    pass
+
+def enterPlayerTile():
+    pass
+
+def whoGoesFirst():
+    pass
+
+def makeMove(board, tile, xstart, ystart):
+    pass
+
+def getBoardCopy(board):
+    pass
+
+def isOnCorner(x, y):
+    pass
+
+def getPlayerMove(board, playerTile):
+    pass
+
+def getComputerMove(board, computerTile):
+    pass
+
+def printScore(board, playerTile, computerTile):
+    pass
+
+def playGame(playerTile, computerTile):
+    pass
