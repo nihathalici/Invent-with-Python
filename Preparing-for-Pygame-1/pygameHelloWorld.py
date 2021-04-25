9ae68fb7009706156dd26227f06ef4fd1422290a
Nihat Halici <nihat.halici@gmail.com>
2021-04-25 01:33:15 -0700

    Create pygameHelloWorld.py

---
+++ Preparing-for-Pygame-1/pygameHelloWorld.py
@@ -0,0 +1,33 @@
+import pygame, sys
+from pygame.locals import *
+
+# Set up pygame.
+pygame.init()
+
+# Set up the window.
+
+# Set up the colors.
+
+# Set up the fonts.
+
+# Set up the text.
+
+# Draw the white background onto the surface.
+
+# Draw a green polygon onto the surface.
+
+# Draw some blue lines onto the surface.
+
+# Draw a blue circle onto the surface.
+
+# Draw a red ellipse onto the surface.
+
+# Draw the text's background rectangle onto the surface.
+
+# Get a pixel array of the surface.
+
+# Draw the text onto the surface.
+
+# Draw the window onto the screen.
+
+# Run the game loop.
