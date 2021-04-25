1eb7ae97ed75399bbfc08d533e85afe97071fbea
Nihat Halici <nihat.halici@gmail.com>
2021-04-25 17:00:35 +0200

    Create animation.py

---
+++ Preparing-for-Pygame-2/animation.py
@@ -0,0 +1,17 @@
+import pygame, sys, time
+from pygame.locals import *
+
+# Set up pygame.
+
+# Set up the window.
+
+# Set up direction variables.
+
+MOVESPEED = 4
+
+# Set up the colors.
+
+# Set up the box data structure.
+
+# Run the game loop.
+
