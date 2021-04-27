c5b8f149bda53bde5bfb7d59dd6d76a7dfec14f2
Nihat Halici <nihat.halici@gmail.com>
2021-04-27 11:11:05 +0200

    Create collisionDetection.py

---
+++ Preparing-for-Pygame-3/collisionDetection.py
@@ -0,0 +1,15 @@
+import pygame, sys, random
+from pygame.locals import *
+
+# Set up pygame.
+pygame.init()
+mainClock = pygame.time.Clock()
+
+# Set up the window.
+
+# Set up the colors.
+
+# Set up the player and food data structures.
+
+# Run the game loop.
+
