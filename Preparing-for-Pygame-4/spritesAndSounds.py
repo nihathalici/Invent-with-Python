2d32f8c2cb188516413dd8f4a9dd14adbbf0a203
Nihat Halici <nihat.halici@gmail.com>
2021-04-28 10:26:34 +0200

    Create spritesAndSounds.py

---
+++ Preparing-for-Pygame-4/spritesAndSounds.py
@@ -0,0 +1,16 @@
+import pygame, sys, time, random
+from pygame.locals import *
+
+# Set up pygame.
+
+# Set up the window.
+
+# Set up the colors.
+
+# Set up the block data structure.
+
+# Set up keyboard variables.
+
+# Set up the music.
+
+# Run the game loop.
