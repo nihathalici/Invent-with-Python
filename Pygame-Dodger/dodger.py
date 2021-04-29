1522d61de8dcab0383b2fee04008e2b6bed9573c
Nihat Halici <nihat.halici@gmail.com>
2021-04-29 12:02:16 +0200

    Create dodger.py

---
+++ Pygame-Dodger/dodger.py
@@ -0,0 +1,24 @@
+import pygame, random, sys
+from pygame.locals import *
+
+def terminate():
+    pass
+
+def waitForPlayerToPressKey():
+    pass
+
+def playerHasHitBaddie(playerRect, baddies):
+    pass
+
+def drawText(text, font, surface, x, y):
+    pass
+
+# Set up pygame, the window, and the mouse cursor.
+
+# Set up the fonts.
+
+# Set up sounds.
+
+# Set up images.
+
+# Show the "Start" screen.
