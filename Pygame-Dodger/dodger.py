86c671008212c8d445acedb5320bfd263bfd1016
Nihat Halici <nihat.halici@gmail.com>
2021-04-29 05:56:39 -0700

    Update dodger.py

--- Pygame-Dodger/dodger.py
+++ Pygame-Dodger/dodger.py
@@ -1,24 +1,61 @@
 import pygame, random, sys
 from pygame.locals import *
 
+WINDOWWIDTH = 600
+WINDOWHEIGHT = 600
+TEXTCOLOR = (0, 0, 0)
+BACKGROUNDCOLOR = (255, 255, 255)
+FPS = 60
+BADDIEMINSIZE = 10
+BADDIEMAXSIZE = 40
+BADDIEMINSPEED = 1
+BADDIEMAXSPEED = 8
+ADDNEWBADDIERATE = 6
+PLAYERMOVERATE = 5
+
 def terminate():
-    pass
+    pygame.quit()
+    sys.exit()
 
 def waitForPlayerToPressKey():
-    pass
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE: # Pressing ESC quits.
+                    terminate()
+                return
 
 def playerHasHitBaddie(playerRect, baddies):
-    pass
+    for b in baddies:
+        if playerRect.colliderect(b['rect']):
+            return True
+    return False
 
 def drawText(text, font, surface, x, y):
-    pass
+    textobj = font.render(text, 1, TEXTCOLOR)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
 
 # Set up pygame, the window, and the mouse cursor.
+pygame.init()
+mainClock = pygame.time.Clock()
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+pygame.display.set_caption('Dodger')
+pygame.mouse.set_visible(False)
 
 # Set up the fonts.
+font = pygame.font.SysFont(None, 48)
 
 # Set up sounds.
+gameOverSound = pygame.mixer.Sound('gameover.wav')
+pygame.mixer.music.load('background.mid')
 
 # Set up images.
+playerImage = pygame.image.load('player.png')
+playerRect = playerImage.get_rect()
+baddieImage = pygame.image.load('baddie.png')
 
 # Show the "Start" screen.
