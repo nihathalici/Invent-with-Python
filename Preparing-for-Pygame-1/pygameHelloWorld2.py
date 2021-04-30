558b3486fc5f658b9ba579632bdf3c5c59075234
Nihat Halici <nihat.halici@gmail.com>
2021-04-30 11:52:48 +0200

    Create pygameHelloWorld2.py

---
+++ Preparing-for-Pygame-1/pygameHelloWorld2.py
@@ -0,0 +1,40 @@
+import pygame, sys
+from pygame.locals import *
+
+# Set up pygame.
+pygame.init()
+
+# Set up the window.
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('¡Hola Mundo!')
+
+# Set up the colors.
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Set up the fonts.
+
+# Set up the text.
+
+# Draw the white background onto the the surface.
+
+# Draw a green polygon onto the surface.
+
+# Draw some blue lines onto the surface.
+
+# Draw a blue circle onto the surface.
+
+# Draw a red ellipse onto the surface.
+
+# Draw the text's background rectangle onto the surface.
+
+# Get a pixel array of the surface.
+
+# Draw the text onto the surface.
+
+# Draw the window onto the screen.
+
+# Run the game loop.
