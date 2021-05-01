c4e8dbee5ca43105e3011985397985461e583e7f
Nihat Halici <nihat.halici@gmail.com>
2021-05-01 10:09:09 -0700

    Update pygameHelloWorld3.py

--- Preparing-for-Pygame-1/pygameHelloWorld3.py
+++ Preparing-for-Pygame-1/pygameHelloWorld3.py
@@ -1,1 +1,26 @@
+import pygame, sys
+from pygame.locals import *
+
+# Set up pygame.
+
+# Set up the window.
+
+# Set up the colors.
+
+# Set up the fonts.
+
+# Set up the text.
+
+# Draw the white background onto the surface.
+
+# Draw a green polygon onto the surface.
+
+# Draw some blue lines onto the surface.
+
+# Draw a blue circle onto the surface.
+
+# Draw a red ellipse onto the surface.
+
+# Draw the text's background rectangle onto the surface.
+
 
