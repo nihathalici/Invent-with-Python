0cf1051af259b39e0d1ccd6b7b67d13d70771097
Nihat Halici <nihat.halici@gmail.com>
2021-05-09 12:45:10 -0700

    Create animation3.py

---
+++ Preparing-for-Pygame-2/animation3.py
@@ -0,0 +1,15 @@
+import pygame, sys, time
+from pygame.locals import *
+
+# Set up pygame.
+pygame.init()
+
+# Set up the window.
+
+# Set up direction variables.
+
+# Set up the colors.
+
+# Set up the box data structure.
+
+# Run the game loop.
