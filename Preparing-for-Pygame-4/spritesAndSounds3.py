78e84d069dc05bd4a1f98a4d767fba690db75820
Nihat Halici <nihat.halici@gmail.com>
2021-05-13 05:16:51 -0700

    Update spritesAndSounds3.py

--- Preparing-for-Pygame-4/spritesAndSounds3.py
+++ Preparing-for-Pygame-4/spritesAndSounds3.py
@@ -1,13 +1,43 @@
+import pygame, sys, time, random
+from pygame.locals import *
+
 # Set up pygame.
+pygame.init()
+mainClock = pygame.time.Clock()
 
 # Set up the window.
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 400
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+pygame.display.set_caption('Sprites and Sounds - 3')
 
 # Set up the colors.
+WHITE = (255, 255, 255)
 
 # Set up the block data structure.
+player = pygame.Rect(300, 100, 40, 40)
+playerImage = pygame.image.load('player.png')
+playerStretchedImage = pygame.transform.scale(playerImage, (40, 40))
+foodImage = pygame.image.load('cherry.png')
+foods = []
+for i in range(20):
+    foods.append(pygame.Rect(random.randint(0, WINDOWWIDTH - 20), random.randint(0, WINDOWHEIGHT - 20), 20, 20))
+
+foodCounter = 0
+NEWFOOD = 40
 
 # Set up keyboard variables.
+moveLeft = False
+moveRight = False
+moveUp = False
+moveDown = False
+
+MOVESPEED = 6
 
 # Set up the music.
+pickUpSound = pygame.mixer.Sound('pickup.wav')
+pygame.mixer.music.load('background.mid')
+pygame.mixer.music.play(-1, 0.0)
+musicPlaying = True
 
 # Run the game loop.
