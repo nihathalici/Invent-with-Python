331cd161c879adc73d7fbdfbf57bf805f07f798f
Nihat Halici <nihat.halici@gmail.com>
2021-05-12 23:21:40 -0700

    Create spritesAndSounds3.py

---
+++ Preparing-for-Pygame-4/spritesAndSounds3.py
@@ -0,0 +1,13 @@
+# Set up pygame.
+
+# Set up the window.
+
+# Set up the colors.
+
+# Set up the block data structure.
+
+# Set up keyboard variables.
+
+# Set up the music.
+
+# Run the game loop.
